464faf43de31ce7ca4e90309569ba8bd7086f4ca
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-06-13 19:20:47 -0300

    show inputs in pass

--- script.js
+++ script.js
@@ -90,3 +90,22 @@ function calculaIdade(dataNasc){
     console.log (formPag.value)
 }
 
+
+
+// const nomeData = document.querySelector('#nome-data');
+// const segNomeData = document.querySelector('#segundo-nome-data');
+// const origemData = document.querySelector('#origem-data');
+// const numInscData = document.querySelector('#num-insc-data');
+
+// btnEnv.addEventListener('click', showMe);
+
+// // Chamar valores dos inputs
+// function showMe() {
+
+// nomeData.innerHTML= nome.value;
+// segNomeData.innerHTML= segNome.value;
+// origemData.innerHTML= origem.value;
+// numInscData.innerHTML= `Nº 0000${numInsc.value}`;
+
+// }
+
