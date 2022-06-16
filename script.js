a242e09e82437b7de95589a52a86fd8b21881d75
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-06-15 22:09:48 -0300

    o começo de um servidor com muitos erros

--- script.js
+++ script.js
@@ -21,86 +21,133 @@ const pagMao = document.querySelector('#pag-mao');
 
 const btnCon = document.querySelector('#btn-concluir');
 
+// const inscricao = {
+//     getValues() {
+//         return {
+//             nome: nome.value,
+//             data: nome.value,
+//             telefone: nome.value,
+//             : nome.value,
+//             nome: nome.value,
+//             nome: nome.value,
+//             nome: nome.value,
+//             nome: nome.value,
+//             nome: nome.value
+//         }
+//     }
+// }
 
-const nomeConf = document.querySelector('#nome-conf');
-
-
-// Calcular idade --------------
-function calculaIdade(dataNasc){ 
-    let dataAtual = new Date();
-    let anoAtual = dataAtual.getFullYear();
-    let anoNascParts = dataNasc.split('-');
-    let diaNasc =anoNascParts[2];
-    let mesNasc =anoNascParts[1];
-    let anoNasc =anoNascParts[0];
-    let idade = anoAtual - anoNasc;
-    let mesAtual = dataAtual.getMonth() + 1; 
-
-        //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
-        if(mesAtual < mesNasc){
-            idade--; 
-        } else {
-        //Se estiver no mes do nascimento, verificar o dia
+const Utils = {
+    
+    // Calcular idade
+    calculaIdade(dataNasc){ 
+        let dataAtual = new Date();
+        let anoAtual = dataAtual.getFullYear();
+        let anoNascParts = dataNasc.split('-');
+        let diaNasc =anoNascParts[2];
+        let mesNasc =anoNascParts[1];
+        let anoNasc =anoNascParts[0];
+        let idade = anoAtual - anoNasc;
+        let mesAtual = dataAtual.getMonth() + 1; 
+    
+            //Se mes atual for menor que o nascimento, nao fez aniversario ainda;  
+            if(mesAtual < mesNasc){
+                idade--; 
+            } else {
+            //Se estiver no mes do nascimento, verificar o dia
             if(mesAtual == mesNasc){ 
                 if(new Date().getDate() < diaNasc ){ 
                 //Se a data atual for menor que o dia de nascimento ele ainda nao fez aniversario
                 idade--; 
                 }
             }
-        } 
-    return idade; 
+        } return idade; 
+    }
 }
 
-    // Mostrar arquivo selecionado do input file
-    Array.prototype.forEach.call(document.querySelectorAll('.file-upload__button'), function (button) {
-        const hiddenInput = button.parentElement.querySelector('.file-upload__input');
-        const label = button.parentElement.querySelector('.file-upload__label');
-        const defaultLabelText = 'Nenhum arquivo selecionado.';
+// Mostrar arquivo selecionado do input file
+Array.prototype.forEach.call(document.querySelectorAll('.file-upload__button'), function (button) {
+    const hiddenInput = button.parentElement.querySelector('.file-upload__input');
+    const label = button.parentElement.querySelector('.file-upload__label');
+    const defaultLabelText = 'Nenhum arquivo selecionado.';
 
-        // Set default text for label
-        label.textContent = defaultLabelText;
-        label.title = defaultLabelText;
+    // Set default text for label
+    label.textContent = defaultLabelText;
+    label.title = defaultLabelText;
 
-        button.addEventListener('click', function () {
-            hiddenInput.click();
-        });
+    button.addEventListener('click', function () {
+        hiddenInput.click();
+    })
 
-        hiddenInput.addEventListener('change', function () {
-            const filenameList = Array.prototype.map.call(hiddenInput.files, function (file) {
-                return file.name
-            });
+    hiddenInput.addEventListener('change', function () {
+        const filenameList = Array.prototype.map.call(hiddenInput.files, function (file) {
+            return file.name
+        })
 
-            label.textContent = filenameList.join() || defaultLabelText;
-            label.title = label.textContent;
-        });
-    });
+        label.textContent = filenameList.join() || defaultLabelText;
+        label.title = label.textContent;
+    })
+})
 
-   btnCon.addEventListener('click', showMe);
-    // Chamar valores dos inputs
-   function showMe() {
+const Form = {
+    nome: nome,
+    nascimento: nascimento,
+    telefone: tel,
+    email: email,
+    localidade: local,
+    autorizacao: textFile,
+    modalidadePag: modalPag,
+    formaPag: formPag,
 
+    getValues() {
+        return {
+            nome: Form.nome.value,
+            nascimento: Form.nascimento.value,
+            telefone: Form.tel.value,
+            email: Form.email.value,
+            localidade: Form.local.value,
+            autorizacao: Form.textFile.value,
+            modalidade_pag: Form.modalidadePag.value,
+            forma_pag: Form.formaPag.value
+        }
+    },
+
+
+    submit(event) {
+        event.preventDefault();
+    }
+}
+
+
+btnCon.addEventListener('click', showMe);
+
+// Chamar valores dos inputs
+function showMe() {
     // converter data americana para data brasileira
     let dataBr = nascimento.value.split('-').reverse().join('/')
 
-    console.log(nome.value);
-    console.log(`${dataBr} | ${calculaIdade(nascimento.value)} anos`);
-    console.log(tel.value);
-    console.log(email.value);
-    console.log(local.value);
-    console.log(textFile.textContent);
-    console.log (modalPag.value);
-    console.log (formPag.value);
+    Form.getValues()
+
+    // console.log(nome.value);
+    // console.log(`${dataBr} | ${Utils.calculaIdade(nascimento.value)} anos`);
+    // console.log(tel.value);
+    // console.log(email.value);
+    // console.log(local.value);
+    // console.log(textFile.textContent);
+    // console.log (modalPag.value);
+    // console.log (formPag.value);
     
-    sessionStorage.setItem('nome', nome.value);
-    sessionStorage.setItem('nascimento', `${dataBr} | ${calculaIdade(nascimento.value)} anos`);
-    sessionStorage.setItem('tel', tel.value);
-    sessionStorage.setItem('email', email.value);
-    sessionStorage.setItem('local', local.value);
-    sessionStorage.setItem('autorizacao', textFile.textContent);
-    sessionStorage.setItem('mod-pag', modalPag.value);
-    sessionStorage.setItem('form-pag', formPag.value);
+    // sessionStorage.setItem('nome', nome.value);
+    // sessionStorage.setItem('nascimento', `${dataBr} | ${calculaIdade(nascimento.value)} anos`);
+    // sessionStorage.setItem('tel', tel.value);
+    // sessionStorage.setItem('email', email.value);
+    // sessionStorage.setItem('local', local.value);
+    // sessionStorage.setItem('autorizacao', textFile.textContent);
+    // sessionStorage.setItem('mod-pag', modalPag.value);
+    // sessionStorage.setItem('form-pag', formPag.value);
     
 }
+
 
 
 
