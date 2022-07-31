d247829b7d40346eb382234976036cf0814be4de
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-07-31 05:36:27 -0300

    feat: teste de rota (2)

--- app.js
+++ app.js
@@ -11,9 +11,12 @@ const emailPass = process.env.PASS_EMAIL
 const emailTo = process.env.EMAILTO
 
 const express = require('express');
+const product = require('./api/product');
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5050;
+
+app.use("/api/product", product);
 
 app.use(express.static('public'))
 app.use(express.json())
