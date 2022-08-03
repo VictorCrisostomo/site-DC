2ab934814d2a8c0fb8233c18a1375c4bc1c89c11
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-08-03 17:20:02 -0300

    feat: confg btns de pagamento (codigo podre🤢)

--- public/scripts/success-script.js
+++ public/scripts/success-script.js
@@ -1,4 +1,4 @@
-'use strict';
+// 'use strict';
 
 // campos do overlay
 const svg = document.querySelector('#svg')
@@ -27,6 +27,11 @@ const pixInt = document.querySelector('#pix-250');
 const pixMeia = document.querySelector('#pix-125');
 const cartaoCred = document.querySelector('#credito');
 const payHand = document.querySelector('#pay-hands');
+
+const btnInt = document.querySelector('#btn-250')
+const btnMeia = document.querySelector('#btn-125');
+const txtInt = document.querySelector('#txt-250');
+const txtMeia = document.querySelector('#txt-125')
 
 const modSelect = document.querySelector('#mod-select');
 const formSelect = document.querySelector('#form-select');
@@ -70,10 +75,27 @@ const Overlay = {
     },
     numId() {
         return `Nº 0000${captVal.ID}`
+    },
+    downOverlay() {
+        const {x, y, width, height} = svg.viewBox.baseVal;
+        const blob = new Blob([svg.outerHTML], {type: 'image/svg+xml'});
+        const url = URL.createObjectURL(blob);
+        const image = document.createElement('img');
+        image.src =  url;
+        image.addEventListener('load', () => {
+            const canvas = document.createElement('canvas');
+            canvas.width = width;
+            canvas.height = height;
+            const context = canvas.getContext('2d');
+            context.drawImage(image, x, y, width, height);
+            btnDown.href = canvas.toDataURL();
+            URL.revokeObjectURL(url);
+        })
     }
 }
 
 const Pagmento = {
+
     botaoModal() {
         let mod = captVal.modalidade;
         let form = captVal.forma;
@@ -95,7 +117,20 @@ const Pagmento = {
             payHand.classList.remove('hidden')
 
         }
+    },
+    copyPixInt() {
+        new ClipboardJS(btnInt, {
+            container: document.getElementById('modal')
+        });
+    },
+    copyPixMeia() {
+        new ClipboardJS(btnMeia, {
+            container: document.getElementById('modal')
+        });
     }
+
+
+
 }
 
     //  campos de conferencia
@@ -114,27 +149,28 @@ const Pagmento = {
     origemData.innerHTML= Overlay.location();
     numInscData.innerHTML= Overlay.numId();
 
+    Overlay.downOverlay();
+
     // campo de pagamento
     formPG.innerHTML= captVal.forma;
 
     // Modal
     modSelect.innerHTML= captVal.modalidade;
     formSelect.innerHTML= captVal.forma;
-    Pagmento.botaoModal();
 
-const {x, y, width, height} = svg.viewBox.baseVal;
-const blob = new Blob([svg.outerHTML], {type: 'image/svg+xml'});
-const url = URL.createObjectURL(blob);
-const image = document.createElement('img');
-image.src =  url;
-image.addEventListener('load', () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext('2d');
-    context.drawImage(image, x, y, width, height);
-    btnDown.href = canvas.toDataURL();
-    URL.revokeObjectURL(url);
-})
+    Pagmento.botaoModal();
+    Pagmento.copyPixInt();
+    Pagmento.copyPixMeia();
+
+    btnMeia.addEventListener("click", () => {
+        btnMeia.style.backgroundColor = "#44c08a"
+        txtMeia.innerHTML = "Codigo copiado"
+    })
+    btnInt.addEventListener("click", () => {
+        btnInt.style.backgroundColor = "#44c08a"
+        txtInt.innerHTML = "Codigo copiado"
+    })
+
+
 
 console.log('Cheguei Papai!!!')
